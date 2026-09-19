091801d717afbc5bf17c60e4cb95522f326a020a
samcordero33-ai <samcordero33@gmail.com>
2026-09-19 00:38:24 -0500

    Refactor build.gradle.kts to simplify plugin configuration
    Removed plugin management and dependency resolution sections.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,5 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.google.dagger.hilt.android") version "2.50" apply false
 }
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-rootProject.name = "SmartClock-app"
-include(":app")
